8ee0cf09ddd347e04318949345b914709d60480f
wbasp <maad7445@gmail.com>
2023-06-19 03:31:52 +0600

    SignUp functions apply and done in Register comp

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,42 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './Login.css';
 import app from '../../firebase/firebase.init';
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { Link } from 'react-router-dom';
-
-const auth = getAuth(app);
+import { AuthContext } from '../../contexts/UserContext';
 
 const Login = () => {
+    const [err, setErr] = useState(null);
+    const createUser = useContext(AuthContext);
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        const confirm = event.target.confirm.value;
+
+        if(password.length < 6){
+            setErr('password should be 6 characters or more.');
+            return;
+        }
+
+        if(password !== confirm){
+            setErr("Your password didn't match");
+            return;
+        }
+
+        createUser(email, password)
+        .then(result =>{
+            console.log(result.user);
+            event.target.reset();
+        })
+        .catch(error => console.error(error));
+    }
 
     return (
         <div className='form-container'>
             <h1 className='login-title'>Please Login</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-control">
                     <label htmlFor='email'>Email</label>
                     <input type='email' name='email' placeholder='your email' required id=''></input>
@@ -22,7 +47,8 @@ const Login = () => {
                 </div>
                 <input className='submit-but' type="submit" value='Login' />
             </form>
-            <p className='log-bottom-txt'>You don't have an account? Go to <Link to='/register'>Register</Link></p>   
+            <p className='log-bottom-txt'>You don't have an account? Go to <Link to='/register'>Register</Link></p>
+            <p className='txt-err'>{err}</p>
         </div>
     );
 };
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './Register.css';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/UserContext';
 
 const Register = () => {
+    const [err, setErr] = useState(null);
+    const createUser = useContext(AuthContext);
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        const confirm = event.target.confirm.value;
+
+        if(password.length < 6){
+            setErr('password should be 6 characters or more.');
+            return;
+        }
+
+        if(password !== confirm){
+            setErr("Your password didn't match");
+            return;
+        }
+
+        createUser(email, password)
+        .then(result =>{
+            console.log(result.user);
+            event.target.reset();
+        })
+        .catch(error => console.error(error));
+    }
+
     return (
         <div className='form-container'>
             <h1 className='login-title'>Please Register</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-control">
                     <label htmlFor='email'>Email</label>
                     <input type='email' name='email' placeholder='your email' required id=''></input>
@@ -21,7 +49,8 @@ const Register = () => {
                 </div>
                 <input className='submit-but' type="submit" value='Register' />
             </form>
-            <p className='log-bottom-txt'>Already have an account? Go to <Link to='/login'>Log in</Link></p>   
+            <p className='log-bottom-txt'>Already have an account? Go to <Link to='/login'>Log in</Link></p>
+            <p className='txt-err'>{err}</p>
         </div>
     );
 };
