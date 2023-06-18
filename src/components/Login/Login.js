0dc1df1b1678010f8e1d15e29dff859ce527d7fb
wbasp <maad7445@gmail.com>
2023-06-18 12:18:37 +0600

    Login, Register done with style

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
+import app from '../../firebase/firebase.init';
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { Link } from 'react-router-dom';
+
+const auth = getAuth(app);
 
 const Login = () => {
+
     return (
-        <div>
-            This is LOgoin......
+        <div className='form-container'>
+            <h1 className='login-title'>Please Login</h1>
+            <form>
+                <div className="form-control">
+                    <label htmlFor='email'>Email</label>
+                    <input type='email' name='email' placeholder='your email' required id=''></input>
+                </div>
+                <div className="form-control">
+                    <label htmlFor='password'>Password</label>
+                    <input type='password' name='password' placeholder='password' required id=''></input>
+                </div>
+                <input className='submit-but' type="submit" value='Login' />
+            </form>
+            <p className='log-bottom-txt'>You don't have an account? Go to <Link to='/register'>Register</Link></p>   
         </div>
     );
 };
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Register.css';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            This is RegisterRegisterRegisterRegister
+        <div className='form-container'>
+            <h1 className='login-title'>Please Register</h1>
+            <form>
+                <div className="form-control">
+                    <label htmlFor='email'>Email</label>
+                    <input type='email' name='email' placeholder='your email' required id=''></input>
+                </div>
+                <div className="form-control">
+                    <label htmlFor='password'>Password</label>
+                    <input type='password' name='password' placeholder='password' required id=''></input>
+                </div>
+                <div className="form-control">
+                    <label htmlFor='confirm'>Confirm Password</label>
+                    <input type='password' name='confirm' placeholder='password' required id=''></input>
+                </div>
+                <input className='submit-but' type="submit" value='Register' />
+            </form>
+            <p className='log-bottom-txt'>Already have an account? Go to <Link to='/login'>Log in</Link></p>   
         </div>
     );
 };
